b8315d94f5606f008bc7bf123421d5a34c72f76a
Choudhury Iqbal <ChoudhuryIqbal@users.noreply.github.com>
2022-01-02 11:22:57 -0500

    Setting up H2 Mock database

---
+++ EmployeeProject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+Create table employee (id int primary key, employee_name varchar(50) ,employee_age int , employee_salary int, department varchar(50)) ;
+insert into employee values (1,'Shabab',30,10000, 'HR');
+insert into employee values (2,'Begum',56,12000,'HouseWife');
